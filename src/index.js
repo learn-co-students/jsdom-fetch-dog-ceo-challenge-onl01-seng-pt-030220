1b7299aabb80d5e59f929f12859c6080fdaede92
github username <github email address>
2020-09-21 08:35:37 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -46,24 +46,24 @@ function changeColor(breed) {
     })
 }
 
-function filterFunction() {
-    let dogList = document.getElementById("dog-breeds")
-    let breedDropDown = document.getElementById("breed-dropdown")
-    breedDropDown.addEventListener("change", (e) => {
-        let letter = e.target.value
-        let filter = breeds.filter(breed => breed.startsWith(letter))
-        dogList.innerHTML = ""
-        renderBreeds(filter)
-    })
-}
-// let dogList = document.getElementById("dog-breeds")
-// let breedDropDown = document.getElementById("breed-dropdown")
-// breedDropDown.addEventListener("change", function(e) {
-//     let letter = e.target.value
-//     let filter = breeds.filter(breed => breed.startsWith(letter))
-//     dogList.innerHTML = ""
-//     renderBreeds(filter)
-// })
+// function filterFunction() {
+//     let dogList = document.getElementById("dog-breeds")
+//     let breedDropDown = document.getElementById("breed-dropdown")
+//     breedDropDown.addEventListener("change", (e) => {
+//         let letter = e.target.value
+//         let filter = breeds.filter(breed => breed.startsWith(letter))
+//         dogList.innerHTML = ""
+//         renderBreeds(filter)
+//     })
+// }
+let dogSelect = document.getElementById("dog-breeds")
+let breedDropDown = document.getElementById("breed-dropdown")
+breedDropDown.addEventListener("change", function(e) {
+    let letter = e.target.value
+    let filter = breeds.filter(breed => breed.startsWith(letter))
+    dogSelect.innerHTML = ""
+    renderBreeds(filter)
+})
 
 document.addEventListener('DOMContentLoaded', function() {
    fetchImages();
